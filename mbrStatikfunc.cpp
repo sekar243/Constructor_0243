8033d9e443e14f98935c14c418520a1ef2d2f9ad
sekar243 <kinasekin6602@gmail.com>
2025-05-17 15:08:49 +0700

    menampilkan data mahasiswa

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -23,3 +23,10 @@ int mahasiswa::nim=0;
 void mahasiswa::setID(){
     id = ++nim;
 }
+
+void mahasiswa::printAll(){
+    cout<<"ID ="<<id<<endl;
+    cout<<"Nama = "<<nama<<endl;
+    cout<<endl;
+}
+
